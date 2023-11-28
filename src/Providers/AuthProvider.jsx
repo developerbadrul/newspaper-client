7138289c6f19e25eb3e47798a2098582eb97e671
--delete <--delete>
2023-11-28 12:03:46 +0600

    store jwt token in local Storage

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,13 +2,14 @@
 import { createContext, useEffect, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword, signOut, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { firebaseApp } from "../firebase.init";
+import useAxiousPublic from "../Hooks/useAxiousPublic";
 
 export const AuthContex = createContext(null)
 
 const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null)
     const auth = getAuth(firebaseApp)
-
+    const axiousPublic = useAxiousPublic();
 
     const googleProvider = new GoogleAuthProvider();
 
@@ -32,11 +33,23 @@ const AuthProvider = ({ children }) => {
         const unSubcribe = onAuthStateChanged(auth, user => {
             console.log("current user", user);
             setCurrentUser(user)
+            if (user) {
+                const userInfo = { email: user.email }
+                axiousPublic.post("/jwt", userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem("access-token", res.data.token)
+                        }
+                        else {
+                            localStorage.removeItem("access-token")
+                        }
+                    })
+            }
         });
         return () => {
             unSubcribe()
         }
-    }, [auth])
+    }, [auth, axiousPublic])
 
     const value = {
         currentUser,
